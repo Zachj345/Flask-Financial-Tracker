324809284b85e338a8ee1010076885b42b901f4a
Zachary Jordan <94946746+Zachj345@users.noreply.github.com>
2022-05-04 14:11:33 -0400

    Update models.py

--- website/models.py
+++ website/models.py
@@ -10,9 +10,9 @@ class Inventory(db.Model):
     large = db.Column(db.Integer)
     xl = db.Column(db.Integer)
     xxl = db.Column(db.Integer)
-    piece_id = db.Column(db.Integer, db.ForeignKey('piece.id'))
-    piece = db.relationship('Piece', back_populates='inventory')
-
+    piece_id = db.Column(db.Integer, db.ForeignKey('piece.id', ondelete='CASCADE'), nullable=False)
+    piece = db.relationship('Piece', back_populates='inventory', backref='inventory', lazy=True, passive_deletes=True)
+                            
 
 class Piece(db.Model):
     __bind_key__ = 'piece'
@@ -20,12 +20,13 @@ class Piece(db.Model):
     title = db.Column(db.String(150), nullable=False)
     prodcost = db.Column(db.Float, nullable=False)
     price = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    inventory = db.relationship('Inventory', back_populates='piece')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE), nullable=False)
+    inventory = db.relationship('Inventory', back_populates='piece', backref='piece', lazy=True, passive_deletes=True)
 
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(140), unique=True)
     password = db.Column(db.String(140))
-    pieces = db.relationship('Piece')
+    pieces = db.relationship('Piece', backref='user', lazy=True, passive_deletes=True)
+    inventory = db.relationship('Inventory',  backref='user', lazy=True, passive_deletes=True)
